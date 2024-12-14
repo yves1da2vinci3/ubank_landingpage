484c8a5a4628a3d50121ddd859f3c0db39fd2044
yves1da2vinci3 <diomadelacorano@gmail.com>
2024-12-14 11:01:53 +0000

    Update styles in Banner and Newsletter components; change favicon in _app.tsx. Adjusted CSS for title font in Banner, modified color properties in Newsletter's description and button hover state for improved aesthetics. Replaced favicon with a new image for better branding.

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,7 +14,7 @@ export default function App({ Component, pageProps }: AppProps) {
           name="viewport"
           content="minimum-scale=1, initial-scale=1, width=device-width, user-scalable=no"
         />
-        <link rel="shortcut icon" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/ubank.png" />
       </Head>
       <Component {...pageProps} />
     </MantineProvider>
